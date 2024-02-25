1cedc17bc0dbd644ff097f55d5a3a14691e88b73
Daniel_Chinn <danielrchinn@gmail.com>
2024-02-25 14:09:40 -0500

    server fixed

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -46,6 +46,13 @@ const server = http.createServer((req, res) => {
                 return incident.id === `MABOS00${id}`
             })));
         }
+
+        else {
+            // If the request is for an unsupported endpoint, send a not found response
+            res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Missing parameters in URL');
+        }
+
     } else {
         // If the request is for an unsupported endpoint, send a not found response
         res.writeHead(404, { 'Content-Type': 'text/plain' });

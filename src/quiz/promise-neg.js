76615e8ad68e189a53f6211f79152265836e3c8e
Daniel_Chinn <danielrchinn@gmail.com>
2024-03-01 15:31:03 -0500

    need to update changes

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -1,3 +1,26 @@
+function sum2DArray(row) {
+    return new Promise((resolve, reject) => {
+        console.log('Sum called ... ');
+        if(Array.isArray(row)) {
+            setTimeout(() => {
+                let sum = 0;
+                for (let i = 0; i < row.length; i++) {
+                    if (row[i] <= 0) {
+
+                    }
+                }
+            }
+        }
+
+
+Promise.any(negsPerRowPromises)
+    .then((results) => {
+        console.log(results);
+    })
+    .catch((error) => console.log(`Err msg: ${error}`));
+}}
+        
+
 const array2D = [
     [1, 2, 3],
     [4, 5, 6],
